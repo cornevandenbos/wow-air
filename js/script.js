5501cc7ade097e0cfb041710e5cf7f37ebe40f0d
Corné van den Bos <31011816+cornevandenbos@users.noreply.github.com>
2018-12-19 20:02:47 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@
 /*eslint-env browser*/
 
 /*eslint 'no-console': 0*/
+
+// Show filter on click
+var filterButton = document.getElementById('filter');
+
+function showFilter() {
+    var filterForm = document.getElementById('filterForm');
+    filterForm.classList.toggle('show');
+}
+
+filterButton.addEventListener('click', showFilter);
+
+// Show search on click
+
+var searchButton = document.getElementById('search');
+
+function showSearch() {
+    var filterForm = document.getElementById('searchForm');
+    filterForm.classList.toggle('show');
+    document.getElementById('searchStyle').focus();
+}
+
+searchButton.addEventListener('click', showSearch);
